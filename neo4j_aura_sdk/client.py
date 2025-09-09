2b0765082b471dd35fb5ba41ff0183b6d9096893
MAndrews_levi <MAndrews@levi.com>
2025-09-09 09:47:12 -0400

    Remove get-upload-url

--- neo4j_aura_sdk/client.py
+++ neo4j_aura_sdk/client.py
@@ -328,9 +328,6 @@ class AuraClient:
             f"instances/{instanceId}/snapshots/{snapshotId}", model=SnapshotResponse
         )
 
-    async def get_upload_url(self, instanceId: str, size: int, ext: str="tar"):
-        path = "upload"
-    
     async def get_customer_managed_keys(self, tenantId: str = ""):
         path = "customer-managed-keys"
         if tenantId:
